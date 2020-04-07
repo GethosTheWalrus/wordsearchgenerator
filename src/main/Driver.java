7691fc7c02fa318bdccf34a1ad34de334038e07b
Mike Toscano <michael.andrew.toscano@gmail.com>
2020-04-07 14:48:46 -0400

    write to pdf (no actual content yet)

--- src/main/Driver.java
+++ src/main/Driver.java
@@ -1,8 +1,15 @@
 package main;
 
+import java.io.IOException;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+
 public class Driver {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		WordSearch ws = new WordSearch(20,20,10);
 		
@@ -13,7 +20,31 @@ public class Driver {
 			
 		}
 		
+		// set up the pdf to contain the content
 		ws.printWordSearch();
+		PDDocument document = new PDDocument();
+		PDPage page = new PDPage();
+		document.addPage(page);
+		PDPageContentStream contents = new PDPageContentStream(document, page);
+		
+		// write content to the pdf
+		contents.beginText();
+		
+		PDFont font = PDType1Font.HELVETICA_BOLD;
+		contents.setFont(font, 30);
+		
+		contents.newLineAtOffset(50, 700);
+		
+		contents.showText("Hello");
+		
+		contents.endText();
+		
+		contents.close();
+		
+		// close the PDF
+		document.save("files/wordsearch.pdf");
+		System.out.println("PDF created");
+		document.close();
 		
 	}
 
